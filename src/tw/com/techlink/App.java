81fccbb335697e3188af95da9b6cd96f40a1bbd6
GuilinZhu <memory140662@gmail.com>
2017-12-21 13:39:03 +0800

    取消配置檔 改用cmd參數

--- src/tw/com/techlink/App.java
+++ src/tw/com/techlink/App.java
@@ -6,25 +6,78 @@ import tableausoftware.documentation.api.rest.bindings.TableauCredentialsType;
 import tableausoftware.documentation.api.rest.util.RestApiUtils;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @SuppressWarnings("ConstantConditions")
 public class App {
 
     private static RestApiUtils restApiUtils = null;
-
+    private static final String[] REPLACE_ATTR_NAME = {"targetAttr", "targetNewValue", "targetOldValue"};
+    private static final Map<String, String> CONFIG_KEY_NAME = new HashMap<>();
+    static {
+        CONFIG_KEY_NAME.put("-sd", "srcDir");
+        CONFIG_KEY_NAME.put("-od", "outputDir");
+        CONFIG_KEY_NAME.put("-tp", "tabcmdPath");
+        CONFIG_KEY_NAME.put("-pn", "projectName");
+        CONFIG_KEY_NAME.put("-u", "username");
+        CONFIG_KEY_NAME.put("-p", "password");
+        CONFIG_KEY_NAME.put("-s", "server");
+        CONFIG_KEY_NAME.put("-du", "dbUsername");
+        CONFIG_KEY_NAME.put("-dp", "dbPassword");
+        CONFIG_KEY_NAME.put("-r", "replace");
+    }
+    /**
+     * -sd srcDir -od outputDir -tp tabcmdPath
+     * -pn projectName -u username -p password
+     * -s server -du dbUsername -dp dbPassword
+     * -r replace
+     * @param args
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws JDOMException
+     */
     public static void main(String[] args) throws IOException, InterruptedException, JDOMException {
-        Map<String, Object> config = getConfigMap();
-        System.out.println("version: 2017/12/20 start.");
+        Map<String, Object> config = getConfig(args);
+        System.out.println("version: 2017/12/21 start.");
         start(config);
+    }
+
+    private static Map<String, Object> getConfig(String[] args) {
+        Map<String, Object> config = new HashMap<>();
+        List<Map<String, String>> replaces = new ArrayList<>();
+        String key = null;
+        Map<String, String> replace = null;
+        for (int index = 0; index < args.length; index++) {
+            String arg = args[index];
+            if (arg.startsWith("-")) {
+                key = CONFIG_KEY_NAME.get(arg);
+                if (key == null) {
+                    System.err.println("指令錯誤：" + arg);
+                    break;
+                }
+                if (args.length > index + 1) {
+                    if (!args[index + 1].startsWith("-")) {
+                        ++ index;
+                        if (arg.equals("-r")) {
+                            replace = new HashMap<>();
+                            for (int position = 1;index + position < args.length && !args[index].startsWith("-"); position ++) {
+                                replace.put(REPLACE_ATTR_NAME[(position - 1) % REPLACE_ATTR_NAME.length], args[index + position]);
+                            }
+                            replaces.add(replace);
+                            config.put(key, replaces);
+                        } else {
+                            config.put(key, args[index]);
+                        }
+                    }
+                }
+            }
+        }
+        return config;
     }
 
     private static void start(Map<String, Object> config) throws JDOMException, IOException {
         if (config != null) {
-            String action = (String) ((config.get("action") != null)? config.get("action"): "ALL");
-            System.out.println("Action: " + action);
+            String action = "ALL";
             System.out.println("***************************************");
             // 更換
             if (action.equalsIgnoreCase("ALL") ||
@@ -33,7 +86,7 @@ public class App {
                 System.out.println("進行更換作業。");
                 final String srcDir = (String) ((config.get("srcDir") != null) ? config.get("srcDir") : "./");
                 final String outputDir = (String) ((config.get("outputDir") != null) ? config.get("outputDir") : "./output");
-                final List<String> targets = (config.get("targets") != null) ? (List<String>) config.get("targets") : new ArrayList<String>();
+                final List<String> targets = Arrays.asList("tds", "tdsx", "twb", "twbx");
                 final List<Map<String, String>> reaplce = (config.get("replace") != null) ? (List<Map<String, String>>) config.get("replace") : new ArrayList<Map<String, String>>();
                 new ReplaceUtil(reaplce, targets, srcDir, outputDir).start();
             }
@@ -55,20 +108,19 @@ public class App {
                     action.equalsIgnoreCase("PUBLISH")) {
                 System.out.println("tabcmd publish.");
                 final String outputDir = (String) ((config.get("outputDir") != null) ? config.get("outputDir") : "./output");
-                final String name = (String) config.get("name");
                 final String username = (String) config.get("username");
                 final String password = (String) config.get("password");
                 final String dbUsername = (String) config.get("dbUsername");
                 final String dbPassword = (String) config.get("dbPassword");
                 final String server = (String) config.get("server");
-                final List<String> targets = (config.get("targets") != null) ? (List<String>) config.get("targets") : new ArrayList<String>();
+                final List<String> targets = Arrays.asList("tds", "tdsx", "twb", "twbx");
                 final String tabcmdPath = (String) config.get("tabcmdPath");
                 final String projectName = (String) config.get("projectName");
                 final File file = new File(outputDir);
                 restApiUtils = RestApiUtils.getInstance(server);
                 TableauCredentialsType credential = restApiUtils.invokeSignIn(username, password, null);
                 for (String target: targets) {
-                    execPublish(file, target, name, dbUsername, dbPassword, tabcmdPath, projectName, credential, server);
+                    execPublish(file, target, null, dbUsername, dbPassword, tabcmdPath, projectName, credential, server);
                 }
                 restApiUtils.invokeSignOut(credential);
                 restApiUtils = null;
@@ -97,7 +149,7 @@ public class App {
                         String cmd = String.format("\"%s\" publish  \"%s\" %s %s %s %s -o ",
                                 tabcmd,
                                 f.getAbsoluteFile().toString(),
-                                (name != null && name.trim().length() > 0) ? "-n \"" + name + "\"" : "",
+                                "-n \"" + f.getName().substring(0, f.getName().lastIndexOf(".")) + "\"",
                                 (projectName != null && projectName.trim().length() > 0) ? "-r \"" + projectName + "\"" : "",
                                 (dbUsername!= null && dbUsername.trim().length() > 0) ? "--db-username \"" + dbUsername + "\"": "",
                                 (dbPassword!= null && dbPassword.trim().length() > 0) ? "--db-password \"" + dbPassword + "\" -save-db-password": ""
@@ -149,27 +201,5 @@ public class App {
             System.out.println(br.readLine());
         }
     }
-
-    private static Map<String, Object> getConfigMap(){
-        try {
-            File c = new File("config.json");
-            if (c.exists()) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("config.json"), "UTF-8"));
-                StringBuffer json = new StringBuffer();
-                while (br.ready()) {
-                    json.append(br.readLine()).append("\n");
-                }
-                Gson gson = new Gson();
-                return gson.fromJson(json.toString(), Map.class);
-            } else {
-                throw new Exception("'config.json' is not found.");
-            }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-        return null;
-    }
-
-
 
 }
