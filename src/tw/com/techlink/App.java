57789434a22d4c0a4180df1b6aea723a31cf421f
GuilinZhu <memory140662@gmail.com>
2017-12-22 16:43:49 +0800

    編碼

--- src/tw/com/techlink/App.java
+++ src/tw/com/techlink/App.java
@@ -208,6 +208,7 @@ public class App {
     private static int execCmd(String cmd) throws IOException, InterruptedException {
         Runtime runtime = Runtime.getRuntime();
         Process process = runtime.exec(cmd);
+        process.waitFor();
         BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
         while (br.ready()) {
             System.out.println(br.readLine());
